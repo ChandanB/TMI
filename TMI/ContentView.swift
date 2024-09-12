08ebef72fdbddfe957aa6cfeac2d472120c7a914
Chandan Brown <chandanbrown@yahoo.com>
2024-09-12 18:29:13 -0400

    Initial Commit

---
+++ TMI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TMI
+//
+//  Created by Chandan Brown on 9/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TMI/TMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMIApp.swift
+//  TMI
+//
+//  Created by Chandan Brown on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
